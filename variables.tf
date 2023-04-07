8da8fd11b376edc6cc69f1e3e71f0745decdb37d
Pim Snel <post@pimsnel.com>
2023-04-07 17:36:07 +0200

    fix default val for extra s3policies

--- variables.tf
+++ variables.tf
@@ -77,5 +77,5 @@ variable "string_schemas" {
 variable "bucket_policy_addition" {
   description = "Additional S3 policies in Terraform format. Can be derived using jsondecode(iam_policy_document.json)."
   type = any
-  default = {}
+  default = null
 }
